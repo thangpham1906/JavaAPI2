cac491c59126a461ad3f474ecd4d16487304a822
thangpham1906 <thangpham1906@gmail.com>
2020-04-11 17:11:57 +0700

    dsa

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -20,62 +20,62 @@ public class MyController {
         return "This is a test";
     }
 
-    @Autowired
-    public StudentRepository studentRepository;
-    public BaseResponse baseResponse = new BaseResponse();
-
-    @RequestMapping(value = "/getAll", method = RequestMethod.POST)
-    public ResponseEntity<?> getAll(){
-        List<Student> listStudent = (List<Student>) studentRepository.findAll();
-        if(listStudent.size()==0){
-            baseResponse.setStatus(1);
-            baseResponse.setMessage("No Student");
-            listStudent = new ArrayList<>();
-            baseResponse.setResult(listStudent);
-            return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
-        }
-        baseResponse.setStatus(1);
-        baseResponse.setMessage("Success");
-        baseResponse.setResult(listStudent);
-        return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
-    }
-
-    @RequestMapping(value = "/addStudent", method = RequestMethod.POST)
-    public ResponseEntity<?> addStudent(@Valid @RequestBody AddStudentRestRequest request){
-        studentRepository.save(new Student(request.getName(),Integer.valueOf(request.getAge())));
-        baseResponse.setStatus(1);
-        baseResponse.setMessage("Add Student Success");
-        System.out.println("ADd OKEEE");
-        return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
-    }
-
-    @RequestMapping("/get/{id}")
-    public ResponseEntity<?> getStudentById(@PathVariable("id") int id){
-        if(studentRepository.existsById(id)){
-            Student student = studentRepository.findById(id).get();
-            baseResponse.setStatus(1);
-            baseResponse.setMessage("Success");
-            baseResponse.setResult(student);
-            return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
-        }else{
-            baseResponse.setStatus(-1);
-            baseResponse.setMessage("This student not exits");
-            return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
-        }
-    }
-
-
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
-    public ResponseEntity<?> deleteStudent(@PathVariable("id") int id){
-        if(studentRepository.existsById(id)){
-            studentRepository.deleteById(id);
-            baseResponse.setStatus(1);
-            baseResponse.setMessage("Success");
-            return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
-        }else{
-            baseResponse.setStatus(-1);
-            baseResponse.setMessage("This student not exits");
-            return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
-        }
-    }
+//    @Autowired
+//    public StudentRepository studentRepository;
+//    public BaseResponse baseResponse = new BaseResponse();
+//
+//    @RequestMapping(value = "/getAll", method = RequestMethod.POST)
+//    public ResponseEntity<?> getAll(){
+//        List<Student> listStudent = (List<Student>) studentRepository.findAll();
+//        if(listStudent.size()==0){
+//            baseResponse.setStatus(1);
+//            baseResponse.setMessage("No Student");
+//            listStudent = new ArrayList<>();
+//            baseResponse.setResult(listStudent);
+//            return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
+//        }
+//        baseResponse.setStatus(1);
+//        baseResponse.setMessage("Success");
+//        baseResponse.setResult(listStudent);
+//        return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
+//    }
+//
+//    @RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+//    public ResponseEntity<?> addStudent(@Valid @RequestBody AddStudentRestRequest request){
+//        studentRepository.save(new Student(request.getName(),Integer.valueOf(request.getAge())));
+//        baseResponse.setStatus(1);
+//        baseResponse.setMessage("Add Student Success");
+//        System.out.println("ADd OKEEE");
+//        return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
+//    }
+//
+//    @RequestMapping("/get/{id}")
+//    public ResponseEntity<?> getStudentById(@PathVariable("id") int id){
+//        if(studentRepository.existsById(id)){
+//            Student student = studentRepository.findById(id).get();
+//            baseResponse.setStatus(1);
+//            baseResponse.setMessage("Success");
+//            baseResponse.setResult(student);
+//            return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
+//        }else{
+//            baseResponse.setStatus(-1);
+//            baseResponse.setMessage("This student not exits");
+//            return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
+//        }
+//    }
+//
+//
+//    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+//    public ResponseEntity<?> deleteStudent(@PathVariable("id") int id){
+//        if(studentRepository.existsById(id)){
+//            studentRepository.deleteById(id);
+//            baseResponse.setStatus(1);
+//            baseResponse.setMessage("Success");
+//            return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
+//        }else{
+//            baseResponse.setStatus(-1);
+//            baseResponse.setMessage("This student not exits");
+//            return ResponseEntity.status(HttpStatus.OK).body(baseResponse);
+//        }
+//    }
 }
--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,55 +1,55 @@
-package com.example.demo;
-
-import javax.persistence.*;
-
-@Entity
-public class Student {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-
-    public Student() {
-    }
-
-    public Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    private String name;
-
-    private int age;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-}
+//package com.example.demo;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//public class Student {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private int id;
+//
+//    public Student() {
+//    }
+//
+//    public Student(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    private String name;
+//
+//    private int age;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Student{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", age=" + age +
+//                '}';
+//    }
+//}
